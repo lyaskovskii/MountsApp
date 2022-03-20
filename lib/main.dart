4bb5f98cae9fa83ad50c3f54fafb90bfe64f2327
nlyaskovsky <nikita.lan@mail.ru>
2022-03-20 14:41:56 +0300

    Initialized Mount App Project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_codelab_1/screens/loading_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashPage(),
+  ));
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../utility/constants.dart';
+import 'landing_page.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+        const Duration(seconds: 4),
+        () => {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const MountsApp(),
+                ),
+              )
+            });
+
+    return Container(
+      color: mainColor,
+      child: Stack(
+        children: [
+          const Align(
+            alignment: Alignment.center,
+            child: Icon(
+              Icons.terrain,
+              color: Colors.white,
+              size: 90.0,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 80.0),
+              child: const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
